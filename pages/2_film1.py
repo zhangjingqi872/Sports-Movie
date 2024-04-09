7bf33418d3dc6a1564164574d20711769fc30ffe
zhangjingqi872 <69142865+zhangjingqi872@users.noreply.github.com>
2024-04-09 14:13:22 +0800

    Update 2_film1.py

--- pages/2_film1.py
+++ pages/2_film1.py
@@ -90,5 +90,5 @@ with st.sidebar:
                              key="1")
 
     if tabs =='首页':
-        st.switch_page("Sports.py")
+        st.page_link("https://sportspy-hujzhjnbl8wfdyya3bjvkn.streamlit.app/")
     
