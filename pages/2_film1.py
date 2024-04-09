d34d511ec7ad4d08f281228655e779e04f674708
zhangjingqi872 <69142865+zhangjingqi872@users.noreply.github.com>
2024-04-09 14:34:07 +0800

    Update 2_film1.py

--- pages/2_film1.py
+++ pages/2_film1.py
@@ -90,5 +90,5 @@ with st.sidebar:
                              key="1")
 
     if tabs =='首页':
-        st.markdown("""<a><href="https://sportspy-hujzhjnbl8wfdyya3bjvkn.streamlit.app/"></a>""", unsafe_allow_html=True)
+        st.markdown('<p><a href="https://sportspy-hujzhjnbl8wfdyya3bjvkn.streamlit.app/" target="_blank">首页</a></p>', unsafe_allow_html=True)
     
