2fdb397023dffc7e8d3caeb59b5e10f93be1e78f
zhangjingqi872 <69142865+zhangjingqi872@users.noreply.github.com>
2024-04-18 23:11:36 +0800

    Update yolo.py

--- pages/yolo.py
+++ pages/yolo.py
@@ -115,7 +115,7 @@ if option == '运动简介':
 else:
     st.sidebar.markdown(
         f"""
-        <div style="color:#ffdeb9; font-family: 宋体; height: 2320px; border: 1px solid rgba(49, 51, 63, 0.1); border-radius: 1rem; padding: calc(1em - 1px); background-color: rgba(255, 255, 255, 0.2);">
+        <div style="color:#ffdeb9; font-family: 宋体; height: 2120px; border: 1px solid rgba(49, 51, 63, 0.1); border-radius: 1rem; padding: calc(1em - 1px); background-color: rgba(255, 255, 255, 0.2);">
             <p style="margin-bottom: 0;">{development}</p>
         </div>
         """,
