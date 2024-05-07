67061ab4f7d9d8960a8e0b7d1838ba197cf5f808
zhangjingqi872 <69142865+zhangjingqi872@users.noreply.github.com>
2024-05-07 17:40:36 +0800

    Update 2_film1.py

--- pages/2_film1.py
+++ pages/2_film1.py
@@ -20,6 +20,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # ---- Perpare ----
 img_Yolo = Image.open("images/hot.jpg")
+img_Billion = Image.open("images/million dollar baby.jpeg")
 
 # ---- Background ----
 local_css("style/background.css")
@@ -40,6 +41,31 @@ with right_column:
     st.link_button("返回首页", "https://sportspy-hujzhjnbl8wfdyya3bjvkn.streamlit.app/")
 
 # ---- Container with films ----
+with st.container():
+    st.write("---")
+    image_column, text_column, margain_column = st.columns((0.5,2,0.5))
+    with margain_column:
+        st.link_button("进入详情", "https://yolopy-arbcrp8rts4goawzdwfugh.streamlit.app/")
+        
+    with image_column:
+    # insert image
+        st.image(img_Yolo)
+
+    with text_column:
+        st.subheader("热辣滚烫")
+        st.write(
+            """
+            导演: 贾玲&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;类型: 剧情 / 喜剧\n
+            编剧: 贾玲 / 孙集斌 / 刘宏禄 / 郭宇鹏 / 卜钰\n
+            制片国家/地区: 中国大陆\n
+            上映日期: 2024-02-10(中国大陆)
+            """
+        )
+        code = '''主演: 贾玲 / 雷佳音 / 张小斐 / 杨紫 / 沙溢 / 乔杉 / 李雪琴 / 马丽 / 魏翔 / 赵海燕 / 张琪 /
+    沈春阳 /沈涛 / 许君聪 / 卜钰 / 朱天福 / 刘宏禄 / 张泰维 / 郭宇鹏 / 李海银 / 李国麟 /
+    张桂玲 / 孙婉竹 / 赵婷婷 / 法志远 / 姬晴 / 曹贺军 / 刘頔 / 贾文田 / 雷鸣武'''
+        st.code(code, language='txt')
+        
 with st.container():
     st.write("---")
     image_column, text_column, margain_column = st.columns((0.5,2,0.5))
